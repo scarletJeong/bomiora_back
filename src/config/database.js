f4460325716021b089f621f0d9e9f163283c20fe
scarletJeong <oncemylife@naver.com>
2026-04-29 15:33:12 +0900

    db 에 큰 숫자 받아오기 가능하도록

--- src/config/database.js
+++ src/config/database.js
@@ -13,6 +13,8 @@ const pool = mysql.createPool({
   queueLimit: 0,
   enableKeepAlive: true,
   keepAliveInitialDelay: 0,
+  supportBigNumbers: true,
+  bigNumberStrings: true,
   // DATETIME(타임존 없음)을 한국 벽시계(KST)로 해석·직렬화.
   // 'Z'로 두면 "2026-04-09 10:24:14" 가 UTC로 읽혀 API·앱에서 +9h 보정되어 19:24로 깨짐.
   // 글로벌/UTC 저장만 쓸 때는 환경변수 DB_TIMEZONE=Z 등으로 바꿀 수 있음.
