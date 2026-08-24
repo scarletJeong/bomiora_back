915228967f12277d34e149edbc689c872b719ea8
scarletJeong <oncemylife@naver.com>
2026-08-24 10:18:06 +0900

    데이터베이스 : 큐 리밋 0> 50으로 제한

--- src/config/database.js
+++ src/config/database.js
@@ -10,7 +10,7 @@ const pool = mysql.createPool({
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0,
+  queueLimit: 50,
   enableKeepAlive: true,
   keepAliveInitialDelay: 0,
   supportBigNumbers: true,
